382257ffb3a45d0701f653805e86ea81de4e2a01
TitDrev <146919229+TitDrev@users.noreply.github.com>
2023-10-04 09:33:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-def test(var:type) -> None:
+class Item:
+    def __init__(self, code, description, prix_unite, tva): 
+        self.code = code
+        self.description = description
+        self.prix_unite = prix_unite
+        self.tva = tva
 
 if __name__ == "__main__":
-    print("Hello World")
+    import datetime
+
+magasin:str = "BUT MARKET"
+ticket_id:int = 2200
+date = datetime.date.today().strftime("%d/%m/%Y")
+caissier:str = "Lisa"
