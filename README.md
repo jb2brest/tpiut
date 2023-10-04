d6128bf6fc217514082629a8ac57025624982841
Palideb <stevan.guily@etudiant.univ-rennes.fr>
2023-10-04 11:20:38 +0200

    Version 1
    Co-Authored-By: felix-fonseca <146922255+felix-fonseca@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # tpiut
 TP sur la gestion de projet niveau IUT
+
+# Description
+
+Ce programme permet de générer un ticket de caisse.
+Il effectue les calculs de prix en fonction des variables attribuées.
+
+
+# Comment utiliser le programme?
+
+Voici comment utiliser 
+
+```
+python3 ticket.py <"nom du magasin"> <"Nom du vendeur/se"> <"n°série A:nbr_articles A | n°série B:nbr_articles B">
+```
+
+Voici un exemple d'utilisation :
+
+```
+Python3 main.py “But Market” “Lisa” “C01:10|C02:2”
+```
+
+qui affiche en sortie :
+
+![resultat](images\resultat.png)
+
+
+# Comment est initialisée la base de données
+
+|Code article|Description|Prix HT unitaire | TVA |
+| :--------: | :-------:  | :------------: | :--: |
+|    C01     |pack de coca  | 5 | 10% |
+|    C02     |kilo de pdt   | 1 | 10% |
+|    C03     |pack Biscotte | 2 | 10%|
+|    C04     |Café soluble  | 3 |10% |
+|    C05     |Crakers       | 4 | 10% |
+
+# Comment modifier la base de données
+
+Afin de modifier la base de donnée, il vous faudra ouvrir le fichier main.py
+à la 5ème ligne vous trouverez "produit".
+Ajoutez en dessous de cette ligne le produit que vous souhaitez en respectant la nomenclature tel que :
+
+```
+"Code d'article":[Description du produit, prix du produit],
+```
+ATTENTION A NE PAS OUBLIER LA VIRGULE
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from datetime import date
 from re import findall
 
+#Initialisation des produits
 produit : dict = {
     "C01":["Pack de coca",5],
     "C02":["Kilo de pdt",1],
@@ -10,14 +11,17 @@ produit : dict = {
     "C05":["Crackers",4]
 }
 
+#Lecture du nombrede ticket et vidage du fichier
 with open("nb_tickets.txt","r+") as file:
     nb_ticket = file.read()
     file.truncate(1000)
 
+#Vérification des arguments
 def verif():
     if len(sys.argv) != 4:
         raise Exception(SyntaxError, "Nombre d'arguments saisie invalide, arguments attendu : \"nom de la société\", \"Nom du vendeur\", \"Cn:nbproduit|Cn:nbproduit\"")
 
+#Traitements des arguments
 def traitement_donnees() -> dict:
     arg = findall("C\d{1,3}:\d{1,2}", sys.argv[3])
     liste_achat : dict = {}
@@ -25,6 +29,7 @@ def traitement_donnees() -> dict:
         liste_achat[a.split(":")[0]] = int(a.split(":")[1])
     return liste_achat
 
+#Mise en forme de tout les produits dans un tableau avec la TVA calculée
 def calcul_somme(dict : dict) -> list:
     list = []
     for key, value in dict.items():
@@ -34,19 +39,26 @@ def calcul_somme(dict : dict) -> list:
             list.append([produit[key],value,total,total_tva])
     return list
 
+#Affichage du ticket
 def affichage(liste : list):
+    #ticket + 1
     with open("nb_tickets.txt","w") as file:
         file.write(str(int(nb_ticket) + 1))
+    #L'on récupère le nom de l'enseigne
     output : str = sys.argv[1]
+    #L'on affiche le numéro ainsi que la date et le nom du serveur
     output += "\nTicket numéro : " + nb_ticket + "\n\nDate : " + date.today().strftime("%d/%m/%y") + "\n\nVous avez été servi par : " + sys.argv[2] + "\n\nNB\tDesc\t\tHT Unitaire\tTVA\tTotal\n"
     total_ht = 0
+    #Ici une boucle qui affiche les informations pour chaque produit commandé
     for prod in liste:
         total_ht += prod[2]
         output += str(prod[1]) + "\t" + prod[0][0] + "\t" + str(prod[0][1]) + "\t\t10%" + "\t" + str(prod[3]) + "€\n"
+    #Les grands totaux
     output += "\n\t\t\t\tTotal HT\t" + str(total_ht)
     output += "\n\t\t\t\tTotal TVA\t" + str(total_ht*0.1)
     output += "\n\t\t\t\tTotal\t\t" + str(total_ht + total_ht*0.1)
 
+    #fermeture du fichier du numéro de ticket
     file.close()
     print(output)
 
