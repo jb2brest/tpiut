1f037dbc535a8d33a595d027d1d2df1b8cd541e4
Titouan-Bucaille <123418351+Titouan-Bucaille@users.noreply.github.com>
2024-10-01 15:18:44 +0200

    contenu.py

--- contenu.py
+++ contenu.py
@@ -8,6 +8,7 @@ def separation(liste_produit):
     return produit_separee
 produit_separee = separation(entree)
 
+
 """ PRODUITS """
 C01 = ["pack de coca", 5]
 C02 = ["kilo de pdt", 1]
@@ -15,90 +16,98 @@ C03 = ["pack Biscotte", 2]
 C04 = ["Café soluble", 3]
 C05 = ["Crakers", 4]
 
-tableau_final = [["NB","Desc.","HT unitaire","TVA","Total"]]
-total_HT = 0
-total_TVA = 0
-total_final = 0
-tva = "10%"
-
-"""         LISTE PRODUITS          """
-
-for i in produit_separee:
-    #print(i)
-    produit = i.split(":")
-    #print(produit)
-    if produit[0] == "C01":
-        nombre = produit[1]
-        description = C01[0]
-        prix_HT = C01[1]
-        total = prix_HT*(1.1)*int(nombre)
-        tableau_final.append([nombre,description,prix_HT,tva,total])
-        #print(tableau_final)
-
-        total_HT = total_HT + (prix_HT*int(nombre))
-        total_TVA = total_TVA + (int(nombre) * 1.1)
-        total_final = total_final + total
 
 
-    elif produit[0] == "C02":
-        nombre = produit[1]
-        description = C02[0]
-        prix_HT = C02[1]
-        total = prix_HT*(1.1)*int(nombre)
-        tableau_final.append([nombre,description,prix_HT,tva,total])
-        #print(tableau_final)
+def creation_tableaux():
+    """         LISTE PRODUITS          """
 
-        total_HT = total_HT + (prix_HT*int(nombre))
-        total_TVA = total_TVA + (int(nombre) * 1.1)
-        total_final = total_final + total
+    tableau_final = [["NB","Desc.","HT unitaire","TVA","Total"]]
+    total_HT = 0
+    total_TVA = 0
+    total_final = 0
+    tva = "10%"
 
 
-    elif produit[0] == "C03":
-        nombre = produit[1]
-        description = C03[0]
-        prix_HT = C03[1]
-        total = prix_HT*(1.1)*int(nombre)
-        tableau_final.append([nombre,description,prix_HT + "€",tva,total])
-        #print(tableau_final)
+    for i in produit_separee:
+        #print(i)
+        produit = i.split(":")
+        #print(produit)
+        if produit[0] == "C01":
+            nombre = produit[1]
+            description = C01[0]
+            prix_HT = C01[1]
+            total = prix_HT*(1.1)*int(nombre)
+            tableau_final.append([nombre,description,prix_HT,tva,total])
+            #print(tableau_final)
 
-        total_HT = total_HT + (prix_HT*int(nombre))
-        total_TVA = total_TVA + (int(nombre) * 1.1)
-        total_final = total_final + total
-
-
-    elif produit[0] == "C04":
-        nombre = produit[1]
-        description = C04[0]
-        prix_HT = C04[1]
-        total = prix_HT*(1.1)*int(nombre)
-        tableau_final.append([nombre,description,prix_HT,tva,total])
-        #print(tableau_final)
-
-        total_HT = total_HT + (prix_HT*int(nombre))
-        total_TVA = total_TVA + (int(nombre) * 1.1)
-        total_final = total_final + total
+            total_HT = total_HT + (prix_HT*int(nombre))
+            total_TVA = total_TVA + (int(nombre) * 1.1)
+            total_final = total_final + total
 
 
-    elif produit[0] == "C05":
-        nombre = produit[1]
-        description = C05[0]
-        prix_HT = C05[1]
-        total = prix_HT*(1.1)*int(nombre)
-        tableau_final.append([nombre,description,prix_HT,tva,total])
-        #print(tableau_final)
+        elif produit[0] == "C02":
+            nombre = produit[1]
+            description = C02[0]
+            prix_HT = C02[1]
+            total = prix_HT*(1.1)*int(nombre)
+            tableau_final.append([nombre,description,prix_HT,tva,total])
+            #print(tableau_final)
+
+            total_HT = total_HT + (prix_HT*int(nombre))
+            total_TVA = total_TVA + (int(nombre) * 1.1)
+            total_final = total_final + total
 
 
-        total_HT = total_HT + (prix_HT*int(nombre))
-        total_TVA = total_TVA + (int(nombre) * 1.1)
-        total_final = total_final + total
+        elif produit[0] == "C03":
+            nombre = produit[1]
+            description = C03[0]
+            prix_HT = C03[1]
+            total = prix_HT*(1.1)*int(nombre)
+            tableau_final.append([nombre,description,prix_HT + "€",tva,total])
+            #print(tableau_final)
+
+            total_HT = total_HT + (prix_HT*int(nombre))
+            total_TVA = total_TVA + (int(nombre) * 1.1)
+            total_final = total_final + total
+
+
+        elif produit[0] == "C04":
+            nombre = produit[1]
+            description = C04[0]
+            prix_HT = C04[1]
+            total = prix_HT*(1.1)*int(nombre)
+            tableau_final.append([nombre,description,prix_HT,tva,total])
+            #print(tableau_final)
+
+            total_HT = total_HT + (prix_HT*int(nombre))
+            total_TVA = total_TVA + (int(nombre) * 1.1)
+            total_final = total_final + total
+
+
+        elif produit[0] == "C05":
+            nombre = produit[1]
+            description = C05[0]
+            prix_HT = C05[1]
+            total = prix_HT*(1.1)*int(nombre)
+            tableau_final.append([nombre,description,prix_HT,tva,total])
+            #print(tableau_final)
+
+
+            total_HT = total_HT + (prix_HT*int(nombre))
+            total_TVA = total_TVA + (int(nombre) * 1.1)
+            total_final = total_final + total
+    return tableau_final,total_HT,total_TVA,total_final
 
 """         LISTE TOTAL          """
 
+def affichage():
+    for i in range(len(tableau_final)):
+        print(tableau_final[i])
 
-for i in range(len(tableau_final)):
-    print(tableau_final[i])
 
+    print(f"Total HT : {total_HT} €")
+    print(f"Total TVA : {total_TVA} €")
+    print(f"Total : {total_final} €")
 
-print(f"Total HT : {total_HT} €")
-print(f"Total TVA : {total_TVA} €")
-print(f"Total : {total_final} €")
+tableau_final,total_HT,total_TVA,total_final = creation_tableaux()
+affichage()
