27873c1912bdfa1cd55bada642a50934fe66f9c6
Levan MARTIN <iamspiizen@gmail.com>
2024-10-02 14:14:42 +0200

    Ajout de la base de données

---
+++ main.py
@@ -0,0 +1,11 @@
+# Déclaration des variables
+items: dict
+
+# Définition des variables
+items = {"C01": ["Pack de coca", 5], 
+         "C02": ["Kilo de pomme de terre", 1],
+         "C03": ["Pack de biscottes", 2],
+         "C04": ["Café soluble", 3],
+         "C05": ["Crackers", 4]
+         }
+
