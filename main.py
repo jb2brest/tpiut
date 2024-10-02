7af23998aae492f3fd02d97c8e6b90b8936437b0
pierrevolle <pierre.volle@etudiant.univ-rennes1.fr>
2024-10-02 17:43:20 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,34 +2,36 @@ import sys
 import datetime
 from random import randint
 
-dict = {'C01':["pack de coca",5],'C02':["kilo de pdt",1],'C03':["pack Biscotte",2],'C04':["Café soluble",3],'C05':["Crackers",4]}
+dict = {'C01':["pack de coca","2 kg",5,0.2],'C02':["kilo de pdt","1 kg",1,0.1],'C03':["pack Biscotte","950 g",2,0.1],'C04':["Café soluble","250 g",3,0.1],'C05':["Crackers","125 g",4,0.2],'C06':["Eau","1.5 L",6,0.1],'C07':["Pain","250 g",1,0.1]}
+
+def calcul_poids(dict, id, quantite):
+    poids = dict[id][1].split(" ")
+    poids_int = int(poids[0])
+    poids_str = f"{poids_int} {poids[1]}"
+    return (poids_int*quantite, poids_str)
+
+def calc_poids(dict, articles):
+    poids_art = {}
+    for article in articles:
+        id_article, quantite_article = article.split(":")
+        assert id_article != ""
+        assert quantite_article != ""
+        quantite_article = int(quantite_article)
+        poids_article, poids_article_str = calcul_poids(dict, id_article, quantite_article)
+        poids_art[id_article] = [poids_article, poids_article_str]
+    return poids_art
 
 def calcul_prix(dict, id, quantite):
-    """
-    Calcule les différents prix pour un article donné.
-    
-    dict: Dictionnaire contenant les informations des articles
-    id: Identifiant de l'article
-    quantite: Quantité de l'article
-    :return: Tuple contenant le prix HT, la TVA, le prix total TTC et le prix total HT
-    """
     assert(type(id)) == str
     assert(type(quantite)) == int
-    tva = 0.1
-    prix_ht = dict[id][1]
+    tva = dict[id][3]
+    prix_ht = dict[id][2]
     prix_tva = prix_ht * tva
     prix_total_tva = (prix_ht + prix_tva) * quantite
     prix_total_ht = prix_ht * quantite
     return (prix_ht, prix_tva, prix_total_tva, prix_total_ht)
 
 def parser_articles(dict, articles):
-    """
-    Parse la liste des articles commandés et calcule les détails pour chaque article.
-    
-    dict: Dictionnaire contenant les informations des articles
-    articles: Liste des articles commandés
-    :return: Dictionnaire contenant les détails de chaque article commandé
-    """
     assert(type(articles)) == list
     ticket = {}
     for article in articles:
@@ -42,14 +44,7 @@ def parser_articles(dict, articles):
         ticket[id_article] = [quantite_article, description_produit, prix_ht, prix_tva, prix_total_tva, prix_total_ht]
     return ticket
 
-def afficher_ticket(nom_ticket, nom_serveur, ticket):
-    """
-    Affiche le ticket de caisse avec tous les détails.
-    
-    nom_ticket: Nom du ticket
-    nom_serveur: Nom du serveur
-    ticket: Dictionnaire contenant les détails de chaque article commandé
-    """
+def afficher_ticket(nom_ticket, nom_serveur, poids_dict, ticket):
     assert(type(nom_ticket)) == str
     assert(type(nom_serveur)) == str
     print(f'''
@@ -61,32 +56,56 @@ Date : {datetime.date.today().strftime("%d/%m/%Y")}
 Vous avez été servi par : {nom_serveur}
 ''')
 
-    print(f"{'NB':<8} {'Desc.':<20} {'HT unitaire':<15} {'TVA':<8} {'Total':<8}") # les ':<8' permettent de créer des colonnes avec un certains nombre de caractères
+    print(f"{'NB':<8} {'Desc.':<20} {'Pds/vol. unitaire':<20} {'Pds/vol. total':<20} {'HT unitaire':<15} {'TVA':<8} {'Total':<8}")
 
     total_ht = 0
     total_tva = 0
     total = 0
+    poids_total = 0
 
     for id_article, details in ticket.items():
         quantite, description, prix_ht, prix_tva, prix_total_tva, prix_total_ht = details
         total_ht += prix_total_ht
         total_tva += prix_tva * quantite
         total += prix_total_tva
-        print(f"{quantite:<8} {description:<20} {prix_ht}€{' '*11} {int(prix_tva/prix_ht*100)}%{' '*5} {prix_total_tva:.1f}€")
+        poids = poids_dict[id_article][1]
+        poids_total += poids_dict[id_article][0]
 
-    print(f"\n{' '*44}Total HT{' '*4}{total_ht:.1f}€") # ici le {' '*4} indique le nombre d'espace à ajouter
-    print(f"{' '*44}Total TVA{' '*3}{total_tva:.1f}€")
-    print(f"{' '*44}Total{' '*7}{total:.1f}€")
+        poids_total_str = f"{poids_dict[id_article][0]}{poids.split(' ')[1]}"
+        print(f"{quantite:<8} {description:<20} {poids:<20} {poids_total_str:<20} {prix_ht}€{' '*12} {int(prix_tva/prix_ht*100)}%{' '*6} {prix_total_tva:.1f}€")
+
+    print(f"\n{' '*85}Total HT{' '*4}{total_ht:.1f}€")
+    print(f"{' '*85}Total TVA{' '*3}{total_tva:.1f}€")
+    print(f"{' '*85}Total{' '*7}{total:.1f}€")
+
+def ajouter_produit(id,desc,poids,prix_ht,tva,dict=dict):
+    has_space=False
+    for elem in poids.split():
+        if elem == " ":
+            has_space=True
+    if has_space==False:
+        for elem in poids.split(" "):
+            if type(elem) == str:
+                elem = f" {elem}"
+    dict[id] = [desc,poids,prix_ht,tva]
 
 if __name__ == "__main__":
-    print(len(sys.argv)) == 4
-    # Afficher les items :
-    print(f"{'Code article':<20} {'Description':<20} {'Prix unitaire hors taxe':<25}")
+    assert len(sys.argv) == 4
+    print(f"{'Code article':<20} {'Description':<20} {'Poids/Vol.':<20} {'Prix unitaire hors taxe':<20} {'TVA':<25}")
 
-    for codeArticle, (description, prix) in dict.items():
-        print(f"{codeArticle:<20} {description:<20} {prix:<25}")
+    for codeArticle, (description, poids, prix, tva) in dict.items():
+        print(f"{codeArticle:<20} {description:<20} {poids:<20} {prix:<20} {tva:<20}")
 
-    # Traiter la commande
     articles = sys.argv[3].split("|")
     ticket = parser_articles(dict, articles)
-    afficher_ticket(sys.argv[1], sys.argv[2], ticket)
+    poids_articles = calc_poids(dict, articles)
+    afficher_ticket(sys.argv[1], sys.argv[2], poids_articles, ticket)
+    ajouter = str(input("Ajouter produit ? (oui/non) : "))
+    if ajouter == "oui":
+        ajout = str(input("Ajouter produit au format suivant : id,desc,poids,prix_ht,tva avec les virgules :"))
+        id,desc,poids,prix_ht,tva = ajout.split(",")
+        ajouter_produit(str(id),str(desc),str(poids),int(prix_ht),float(tva),dict=dict)
+        print(f"{'Code article':<20} {'Description':<20} {'Poids/Vol.':<20} {'Prix unitaire hors taxe':<20} {'TVA':<25}")
+
+        for codeArticle, (description, poids, prix, tva) in dict.items():
+            print(f"{codeArticle:<20} {description:<20} {poids:<20} {prix:<20} {tva:<20}")
