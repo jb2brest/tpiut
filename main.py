9228ca872c44e18a92abe92a0b4234256c22c452
DeSpy3 <98421881+DeSpy3@users.noreply.github.com>
2025-09-30 15:44:47 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+import sys
+from datetime import datetime
+
+# Dictionnaire des articles disponibles
+ARTICLES = {
+    "CO1": {"desc": "pack de coca", "prix": 5, "tva": 0.1},
+    "CO2": {"desc": "kilo de pdt", "prix": 1, "tva": 0.1},
+    "CO3": {"desc": "pack Biscotte", "prix": 2, "tva": 0.1},
+    "CO4": {"desc": "Cafe soluble", "prix": 3, "tva": 0.1},
+    "CO5": {"desc": "Crakers", "prix": 4, "tva": 0.1},
+}
+
+def generer_ticket(magasin, caissier, commande_str):
+    numero_ticket = 2200
+    date_ticket = datetime.now().strftime("%d/%m/%Y")
+
+    lignes_commande = []
+    total_ht = 0
+    total_tva = 0
+
+    # Découper la commande en paires code:quantité
+    items = commande_str.split("|")
+    for i in items:
+        code, quantite = i.split(":")
+        quantite = int(quantite)
+
+        if code not in ARTICLES:
+            print(f"Article {code} inconnu, ignoré.")
+            continue
+
+        article = ARTICLES[code]
+        prix_ht = article["prix"] * quantite
+        montant_tva = prix_ht * article["tva"]
+        prix_ttc = prix_ht + montant_tva
+
+        total_ht += prix_ht
+        total_tva += montant_tva
+
+        lignes_commande.append(
+            f"{quantite}\t{article['desc']:<15}\t{article['prix']}€\t"
+            f"{int(article['tva']*100)}%\t{prix_ttc:.2f}€"
+        )
+
+    # Construction du ticket
+    ticket = [
+        f"\n\t{magasin}",
+        f"Ticket numéro : {numero_ticket}",
+        f"\nDate : {date_ticket}",
+        f"\nVous avez été servi par : {caissier}\n",
+        "NB\tDesc.\t\tHT unitaire\tTVA\tTotal",
+        "\n".join(lignes_commande),
+        "\n",
+        f"\t\t\t\tTotal HT\t{total_ht:.2f}€",
+        f"\t\t\t\tTotal TVA\t{total_tva:.2f}€",
+        f"\t\t\t\tTotal   \t{total_ht + total_tva:.2f}€",
+    ]
+
+    return "\n".join(ticket)
+
+
+ticket = generer_ticket("BUT Market", "Lisa", "CO1:10|CO2:2")
+print(ticket)
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python3 main.py <Magasin> <Caissier> <Commande>")
+        print("Exemple: python3 main.py 'BUT Market' 'Lisa' 'C01:10|C02:2'")
+        sys.exit(1)
+
+    magasin = sys.argv[1]
+    caissier = sys.argv[2]
+    commande_str = sys.argv[3]
+
+    ticket = generer_ticket(magasin, caissier, commande_str)
+    print(ticket)
