f08c5ff1ffdb51b3c43144e26571b45a592a40a3
DanSTEWARTiutLannion <dan.stewart@etudiant.univ-rennes.fr>
2025-09-30 15:22:13 +0200

    création du dictionnaire

--- projet/main.py
+++ projet/main.py
@@ -1,6 +1,23 @@
+marchandise:dict = {
+    "art1" : {"desc":"pack de coca","HTuni":5,"TVA":0.1},
+    "art2" : {"desc":"kilo de pdt","HTuni":1,"TVA":0.1},
+    "art3" : {"desc":"pack biscotte","HTuni":2,"TVA":0.1}
+}
+
+
+
+
+
+
+
+
+
+
 num_tic:int = 1
 date = "30/09/2025"
-ticket : str = f"BUT Market \nTicket num : {num_tic}\nDate : {date}\n"
+employe :str = "Lisa" 
+ticket1 : str = f"BUT Market \nTicket num : {num_tic}\n\nDate : {date}\nVous avez été desservi par {employe}\n"
 
+ticket2 : str = ""
 
-print (ticket)
+print (ticket1+ticket2)
