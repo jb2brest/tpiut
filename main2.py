71691b80e76488724fc6f7f7e8004caacf98cef4
StudQT <quentin.henon@etudiant.univ-rennes.fr>
2025-10-01 16:38:57 +0200

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,9 @@
+PRODUITS = {
+    "C01": {"nom": "pack de coca", "poids": "2kg", "prix_ht": 5.0, "tva": 0.20, "origine": "Lituanie"},
+    "C02": {"nom": "kilo de pdt", "poids": "1kg", "prix_ht": 1.0, "tva": 0.10, "origine": "Espagne"},
+    "C03": {"nom": "pack Biscotte", "poids": "950g", "prix_ht": 2.0, "tva": 0.10, "origine": "France"},
+    "C04": {"nom": "Café soluble", "poids": "250g", "prix_ht": 3.0, "tva": 0.10, "origine": "Roumanie"},
+    "C05": {"nom": "Crakers", "poids": "125g", "prix_ht": 4.0, "tva": 0.20, "origine": "Angleterre"},
+    "C06": {"nom": "Eau", "poids": "1,5L", "prix_ht": 6.0, "tva": 0.10, "origine": "Suisse"},
+    "C07": {"nom": "Pain", "poids": "250g", "prix_ht": 1.0, "tva": 0.10, "origine": "France"},
+}
