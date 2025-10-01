b3c1d36f041f713f284a946b2744966fbb1f0c26
albanmeallet <alban.meallet56@gmail.com>
2025-10-01 11:50:35 +0200

    nouveau fichier

--- main.py
+++ main.py
@@ -34,11 +34,13 @@ prenom_valid = re.match(r"^[A-Za-zÀ-ÖØ-öø-ÿ\-]+$", prenom)
 articles_valid = re.match(r"^([A-Z0-9]+:\d+)(\|[A-Z0-9]+:\d+)*$", articles)
 
 # liste des objets
-lst_item = {"C01":{"nom":"pack de coca","prix":5},
-            "C02":{"nom":"kilo de pdt","prix":1},
-            "C03":{"nom":"pack Biscotte","prix":2},
-            "C04":{"nom":"Café soluble","prix":3},
-            "C05":{"nom":"Crackers","prix":4}}
+lst_item = {"C01":{"nom":"pack de coca","prix":5,"Poids":"2kg","Origine":"Lituanie","TVA":"20","Poids_kg":2},
+            "C02":{"nom":"kilo de pdt","prix":1,"Poids":"1kg","Origine":"Espagne","TVA":"10","Poids_kg":1},
+            "C03":{"nom":"pack Biscotte","prix":2,"Poids":"950g","Origine":"France","TVA":"10","Poids_kg":0.95},
+            "C04":{"nom":"Café soluble","prix":3,"Poids":"250g","Origine":"Roumanie","TVA":"10","Poids_kg":0.25},
+            "C05":{"nom":"Crackers","prix":4,"Poids":"125g","Origine":"Angleterre","TVA":"20","Poids_kg":0.125},
+            "C06":{"nom":"Eau","prix":6,"Poids":"1,5L","Origine":"Suisse","TVA":"10","Poids_kg":1.5},
+            "C07":{"nom":"Pain","prix":1,"Poids":"250g","Origine":"France","TVA":"10","Poids_kg":0.250},}
 
 articles_liste = articles.split("|")
 # vérifie si les regex sont valides 
@@ -57,20 +59,33 @@ if lieu_valid and prenom_valid and articles_valid:
         print(f"Ticket numéro : {num_ticket} \n")
         print(f"Date : {date}\n")
         print(f"Vous avez été servi par : {prenom}\n")
-        print("NB   Desc.           HT unitaire     TVA     Total")
+        print("NB   Desc.          Poids/Volume unitaire Poids/Volume total HT unitaire     TVA     Total")
         total_HT=0
         total_TTC=0
         total_TVA=0
         for article in articles_liste:
             lst=article.split(":")
-            total_prduit=lst_item[lst[0]]["prix"]*1.1*int(lst[1])
+            if lst_item[lst[0]]["TVA"]==10: # verifie si la TVA est à 10% cela change les calcule
+                total_prduit=lst_item[lst[0]]["prix"]*1.1*int(lst[1])
+                total_TTC+=lst_item[lst[0]]["prix"]*1.1*int(lst[1])
+                total_TVA+=lst_item[lst[0]]["prix"]*int(lst[1])*0.1
+                total_prduit = round(total_prduit,2) #arrondie le nombre calculé
+            else:
+                total_prduit=lst_item[lst[0]]["prix"]*1.2*int(lst[1])
+                total_TTC+=lst_item[lst[0]]["prix"]*1.2*int(lst[1])
+                total_TVA+=lst_item[lst[0]]["prix"]*int(lst[1])*0.2
+                total_prduit = round(total_prduit,2) #arrondie le nombre calculé
             total_HT+=lst_item[lst[0]]["prix"]*int(lst[1])
-            total_TTC+=lst_item[lst[0]]["prix"]*1.1*int(lst[1])
-            total_TVA+=lst_item[lst[0]]["prix"]*int(lst[1])*0.1
-            print(f"{int(lst[1])}    {lst_item[lst[0]]['nom']}       {lst_item[lst[0]]['prix']}         10%         {total_prduit}")
+            poid_art=lst_item[lst[0]]['Poids_kg']*int(lst[1])
+            if poid_art<1:
+                print(f"{int(lst[1])}    {lst_item[lst[0]]['nom']}       {lst_item[lst[0]]['Poids']}                    {poid_art*1000}g           {lst_item[lst[0]]['prix']}               {lst_item[lst[0]]['TVA']}%         {total_prduit}")
+            else:
+                print(f"{int(lst[1])}    {lst_item[lst[0]]['nom']}       {lst_item[lst[0]]['Poids']}                    {poid_art}kg           {lst_item[lst[0]]['prix']}               {lst_item[lst[0]]['TVA']}%         {total_prduit}")
         print("\n")
         print(f"Total HT {total_HT}")
+        total_TVA = round(total_TVA,2)
         print(f"Total TVA {total_TVA}")
+        total_TTC = round(total_TTC,2)
         print(f"Total {total_TTC}") 
     else:
         print("le code d'article que vous avez rentrer n'est pas dans la liste des item.")
--- readme.md
+++ readme.md
@@ -1,4 +1,12 @@
 # Documentation du code
+## Objectifs
+
+Le programme doit prevoir les fonctionnalités suivantes:
+- Demande du nom de la personne
+- Demande du nom du magasin
+- Demande de l'ajout des articles
+- Générer un ticket de caisse prenant en compte les paramètres précédents
+
 ## Comment lancer le programme
 Pour lancer le programme il faut rentrer la ligne suivante dans le terminal: \n
 ```bash
@@ -19,42 +27,49 @@ Après execution du programme, le ticket est imprimé dans la console
 Exemple de ticket :
 ```shell
 But Maket mm44
-Ticket numéro : 39 
+Ticket numéro : 51 
 
 Date : 01/10/2025
 
 Vous avez été servi par : Lisaf
 
-NB   Desc.           HT unitaire     TVA     Total
-1    Crackers       4         10%         4.4
-5    Café soluble       3         10%         16.5
+NB   Desc.          Poids/Volume unitaire Poids/Volume total HT unitaire     TVA     Total
+1    Crackers       125g                    125.0g           4               20%         4.8
+9    Café soluble       250g                    2.25kg           3               10%         32.4
 
 
-Total HT 19
-Total TVA 1.9
-Total 20.9
+Total HT 31
+Total TVA 6.2
+Total 37.2
 ```
 ## Liste des produits disponible et ajout de nouveau article
 #### Article actuellement disponible par défaut
-| Code article  | Description  |  Prix HT unitaire |
-|---|---|---|
-| C01  | Pack de coca  | 5  |
-| C02  | kilo de pdt  | 1  |
-| C03  | pack Biscotte  | 2  |
-| C04  | Café soluble  | 3  |
-| c05  | Crackers  | 4  |
+| Code article  | Description  |  Prix HT unitaire | Poid ou volume unitaire | TVA |
+|---|---|---|---|---|
+| C01  | Pack de coca  | 5  | 2kg  | 20%  |
+| C02  | kilo de pdt  | 1  | 1kg  | 10%  |
+| C03  | pack Biscotte  | 2  | 950g  | 10%  |
+| C04  | Café soluble  | 3  | 250g  | 10%  |
+| C05  | Crackers  | 4  | 125g  | 20%  |
+| C06  | Eau  | 6 | 1.5L  | 10%  |
+| C07  | Pain  | 1  | 250g  | 10%  |
+
+
 
 ### Comment ajouter un article
 il faut modifier cette partie du code :
 ```python
-lst_item = {"C01":{"nom":"pack de coca","prix":5},
-            "C02":{"nom":"kilo de pdt","prix":1},
-            "C03":{"nom":"pack Biscotte","prix":2},
-            "C04":{"nom":"Café soluble","prix":3},
-            "C05":{"nom":"Crackers","prix":4}}
+lst_item = {"C01":{"nom":"pack de coca","prix":5,"Poids":"2kg","Origine":"Lituanie","TVA":"20","Poids_kg":2},
+            "C02":{"nom":"kilo de pdt","prix":1,"Poids":"1kg","Origine":"Espagne","TVA":"10","Poids_kg":1},
+            "C03":{"nom":"pack Biscotte","prix":2,"Poids":"950g","Origine":"France","TVA":"10","Poids_kg":0.95},
+            "C04":{"nom":"Café soluble","prix":3,"Poids":"250g","Origine":"Roumanie","TVA":"10","Poids_kg":0.25},
+            "C05":{"nom":"Crackers","prix":4,"Poids":"125g","Origine":"Angleterre","TVA":"20","Poids_kg":0.125},
+            "C06":{"nom":"Eau","prix":6,"Poids":"1,5L","Origine":"Suisse","TVA":"10","Poids_kg":1.5},
+            "C07":{"nom":"Pain","prix":1,"Poids":"250g","Origine":"France","TVA":"10","Poids_kg":0.250},}
+
 ```
 Et rajouter cette ligne type (en la modifiant):
 ``` python
-"C06":{"nom":"produit","prix":0}
+"C07":{"nom":"article","prix":0,"Poids":"0g","Origine":"France","TVA":"0","Poids_kg":0}
 ```
 n'oubilez pas de rajouter la virgule sur l'avant dernière article
