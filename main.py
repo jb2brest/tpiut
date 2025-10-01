b1287e093576746122e885fcdc1cb0ab663adcfb
Loann Jegu <loannjegu29@gmail.com>
2025-10-01 10:03:12 +0200

    Ajout Variable num_ticket

--- main.py
+++ main.py
@@ -16,8 +16,15 @@ def calcule_TVA(prix_ht) -> int:
 if len(sys.argv) != 4:  # 4 car sys.argv[0] = nom du script + 3 paramètres
     print("Usage: python script.py nom_du_magasin nom_du_vendeur liste_des_articles")
     sys.exit(1)
+<<<<<<< Updated upstream
 
 num_ticket=0
+=======
+    
+f = open('num_ticket.txt', 'r+')
+num_ticket = int(f.read().strip())
+    
+>>>>>>> Stashed changes
 nom_magasin:str = sys.argv[1]
 nom_vendeur:str = sys.argv[2]
 list_articles_brut:str = sys.argv[3]
@@ -44,4 +51,11 @@ for article in list_articles:
 today = datetime.datetime.now()
 date = today.strftime("%d/%m/%y")
 
+<<<<<<< Updated upstream
 print(f"{nom_magasin}\nTicket numéro : {num_ticket}\n\nDate : {date}\n\nVous avez été servi par : {nom_vendeur}\n\n{list_articles}")
+=======
+num_ticket += 1
+f.seek(0)
+f.write(str(num_ticket))
+f.close()
+>>>>>>> Stashed changes
