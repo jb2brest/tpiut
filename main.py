7fd550056b112233ef4889abdd1abb3c151b9e93
Lucas Vieira Miranda <lucas.vieiramiranda12@gmail.com>
2025-10-01 09:13:15 +0200

    Create main.py
    Code python de notre outil

---
+++ main.py
@@ -0,0 +1,21 @@
+# Tableau des articles
+
+articles : dict = {}
+
+articles = {{"Code" : "C01", "Description" : "Pack de coca", "Prix"  : 5}, 
+            {"Code" : "C02", "Description" : "Kilo de pdt", "Prix"  : 1},
+            {"Code" : "C03", "Description" : "Pack de Biscotte", "Prix"  : 2},
+            {"Code" : "C04", "Description" : "Café soluble", "Prix"  : 3},
+            {"Code" : "C05", "Description" : "Crakers", "Prix"  : 4}}
+
+# def des variables
+
+TVA : float = 0.01
+
+Total_TVA : float = 0.0
+Total_HT : float = 0.0
+Total_articles : float = 0.0
+Total_final : float = 0.0
+
+num_ticket : str = "0000"
+Date : str = "//"
