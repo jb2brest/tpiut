9bc05a9b82a9e12783074b79907405b6bed063c6
Lucas Vieira Miranda <lucas.vieiramiranda12@gmail.com>
2025-10-01 09:29:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from datetime import datetime
 
 # Tableau des articles
-articles : dict = {}
-
-articles = {{"Code" : "C01", "Description" : "Pack de coca", "Prix"  : 5}, 
+articles : list
+articles = [{"Code" : "C01", "Description" : "Pack de coca", "Prix"  : 5},
             {"Code" : "C02", "Description" : "Kilo de pdt", "Prix"  : 1},
             {"Code" : "C03", "Description" : "Pack de Biscotte", "Prix"  : 2},
             {"Code" : "C04", "Description" : "Café soluble", "Prix"  : 3},
-            {"Code" : "C05", "Description" : "Crakers", "Prix"  : 4}}
+            {"Code" : "C05", "Description" : "Crakers", "Prix"  : 4}]
 
-# def des variables
+# Définition des variables
 TVA : float = 0.01
-
 Total_TVA : float = 0.0
 Total_HT : float = 0.0
 Total_articles : float = 0.0
 Total_final : float = 0.0
-
 num_ticket : str = "2200"
-Date : str = "//"
+Date : str = ""
+
+
+# fonction Date
+def Date() -> str : 
+    """Fonction qui récupère la date à l'instant T
+    Returns:
+        str: Date au format JJ/MM/YYYY
+    """
+    date : str = datetime.now()
+    Date = date.strftime("%d/%m/%Y")
+    return Date
