32c444073ba906b538b4c959033f6fa4f6be08f0
AndreaH <94178978+AndreaHvd@users.noreply.github.com>
2025-10-01 09:18:13 +0200

    typage

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-produits:dict = {
+produits:list[dict] = [
     {"code_article":"C01", "description":"pack de coca", "prix_HT":5},
     {"code_article":"C02", "description":"kilo de pdt", "prix_HT":1},
     {"code_article":"C03", "description":"pack Biscotte", "prix_HT":2},
     {"code_article":"C04", "description":"Café soluble", "prix_HT":3},
     {"code_article":"C05", "description":"Crackers", "prix_HT":4}
-}
+]
