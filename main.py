caacdbe4a77b9b8052f69f30d18eb8033905201d
Zah0w <129760649+Zah0w@users.noreply.github.com>
2025-10-01 14:47:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,5 @@ liste_produits:list[dict] = [
     {"code_article":"C02", "description":"kilo de pdt","poids":"1 kg" , "prix_HT":1},
     {"code_article":"C03", "description":"pack Biscotte","poids":"950 g" , "prix_HT":2},
     {"code_article":"C04", "description":"Café soluble","poids":"250 g" , "prix_HT":3},
-    {"code_article":"C05", "description":"Crackers", "poids":"125 g" , "prix_HT":4},
-    {"code_article":"C06", "description":"Eau", "poids":"1.5 L", "prix_HT":6},
-    {"code_article":"C07", "description":"Pain", "poids":"250 g", "prix_HT":1}
+    {"code_article":"C05", "description":"Crackers", "poids":"125 g" , "prix_HT":4}
 ]
