f7937322a237cd2519746a83b35ac42e722b0184
AndreaH <94178978+AndreaHvd@users.noreply.github.com>
2025-10-01 09:50:30 +0200

    Liste général

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ produits:list[dict] = [
     {"code_article":"C05", "description":"Crackers", "prix_HT":4}
 ]
 
+def calcule_TVA(prix_ht) -> int:
+    prix_ttc = prix_ht*1.1
+    return prix_ttc
+
 if len(sys.argv) != 4:  # 4 car sys.argv[0] = nom du script + 3 paramètres
     print("Usage: python script.py nom_du_magasin nom_du_vendeur liste_des_articles")
     sys.exit(1)
@@ -27,21 +31,14 @@ for article in list_articles:
     parties = article.split(":")
     code_article = parties[0]
     nb_article = int(parties[1])
-    list_articles.append([code_article, nb_article])
-
-for article in list_articles:
-    code:str = article[0]
-    quantite:int = article[1]
     for i in range (len(produits)):
-        if code == produits(i)("code_article") :
+        if code_article == produits(i)("code_article") :
             prix_HT:int = produits(i)("prix_HT")
-    prix_HT = prix_HT*quantite
-    article.append[]
+            description = produits(i)("description")
+    prix_HT = prix_HT*nb_article
+    total_TVA:float = calcule_TVA(prix_HT)  
+    list_articles.append([nb_article, description, prix_HT, "10%", total_TVA])
 
 today = datetime.datetime.now()
 date = today.strftime("%d/%m/%y")
-
-def calcule_TVA(prix_ht) -> int:
-    prix_ttc = prix_ht*1.1
-    return prix_ttc
 
